5d515bc534b82985036de36dc27380c371658e81
Elaine Thai <ethai1827@gmail.com>
2019-05-17 00:09:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-const app = "I don't do much."
+var dodger = document.getElementById('dodger');
+
+function moveDodgerLeft() {
+  var leftNums = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNums, 10)
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
